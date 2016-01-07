76cee1a06730cf54edfcb6f3ef003de26cb28858
Simen Li <simenkid@gmail.com>
2016-01-07 18:35:36 +0800

    code tested, need final refine

--- timer.lua
+++ timer.lua
@@ -4,19 +4,15 @@
 -- LICENSE: MIT
 -- Simen Li <simenkid@gmail.com>
 ------------------------------------------------------------------------------
-
 local timer = {}
-local _exequeImmed = {}
-local exequeImmed = {}
-local exeque = {}
-local ttbl = {}
+local _exequeImmed, exequeImmed, exeque, ttbl = {}, {}, {}, {}
+local tick, lock = 2, false
 
 timer.enable = false
-timer.id = 0
+timer.id = 6
 
 local function rmEntry(tbl, pred)
     local x, len = 0, #tbl
-
     for i = 1, len do
         local trusy, idx = false, (i - x)
         if (type(pred) == 'function') then trusy = pred(tbl[idx])
@@ -32,14 +28,12 @@ local function rmEntry(tbl, pred)
     return tbl
 end
 
-local lock = false
 local function checkloops()
-    if (lock) then return end
-    lock = true
+    if (lock) then return else lock = true end
     local tobj
     for i, tob in ipairs(ttbl) do
-        tob.delay = tob.delay - 1
-        if (tob.delay == 1) then
+        tob.delay = tob.delay - tick
+        if (tob.delay <= tick) then
             table.insert(exeque, tob)
             ttbl = rmEntry(ttbl, tob)
         end
@@ -48,10 +42,10 @@ local function checkloops()
     for i = 1, #_exequeImmed do table.insert(exequeImmed, _exequeImmed[i]) end
     rmEntry(_exequeImmed, function (v) return v ~= nil end)
 
-    if (#exequeImmed > 0) then
-        -- Immediately execute all targets
+    if (#exequeImmed > 0) then  -- Immediately execute all targets
         for i, immed in ipairs(exequeImmed) do
-            local status, err = pcall(immed.f, unpack(immed.args))
+            local ar = immed.args
+            local status, err = pcall(immed.f, unpack(ar))
             if not (status) then print("Task execution fails: " .. tostring(err)) end
         end
         rmEntry(exequeImmed, function (v) return v ~= nil end)
@@ -63,16 +57,13 @@ local function checkloops()
         timer.enable = false
     end
 
-    if (tobj ~= nil) then
-        -- Re-insert the repeatable tobj to table
+    if (tobj ~= nil) then -- Re-insert the repeatable tobj to table
         if (tobj.rp > 0) then
             tobj.delay = tobj.rp
-            if (tobj.delay == 1) then table.insert(exeque, tobj)
+            if (tobj.delay <= tick) then table.insert(exeque, tobj)
             else table.insert(ttbl, tobj)
             end
         end
-        -- Execute the target
-        -- tobj.f(unpack(tobj.args))
         local status, err = pcall(tobj.f, unpack(tobj.args))
         if not (status) then print("Task execution fails: " .. tostring(err)) end
     end
@@ -80,15 +71,13 @@ local function checkloops()
 end
 
 function timer.start()
-    -- tid = 6, intvl = 1ms, repeat
-    tmr.alarm(timer.id, 1, 1, checkloops)
-    timer.enable = true
+        tmr.alarm(timer.id, 2, 1, checkloops)   -- tid = 6, intvl = 1ms, repeat
+        timer.enable = true
 end
 
 function timer.stop()
     tmr.stop(timer.id)
     timer.enable = false
-
     _exequeImmed = rmEntry(_exequeImmed, function (v) return v ~= nil end)
     exequeImmed = rmEntry(exequeImmed, function (v) return v ~= nil end)
     exeque = rmEntry(exeque, function (v) return v ~= nil end)
@@ -104,41 +93,32 @@ function timer.set(tid)
 end
 
 function timer.setImmediate(fn, ...)
-    local tobj = { delay = 0, f = fn, rp = 0, args = { ... } }
+    local tobj = { delay = 0, f = fn, rp = 0, args = {...} }
     table.insert(_exequeImmed, tobj)
-
     if (timer.enable == false) then timer.start() end
     return tobj
 end
 
 function timer.setTimeout(fn, delay, ...)
-    print('set timeout 1')
-    local tobj = { delay = delay, f = fn, rp = 0, args = { ... } }
-    print('set timeout 2')
-    if (delay < 2 or delay > 2147483646) then
-        print('set timeout 3')
-        tobj.delay = 1
+    print('set timeout args')
+    print(...)
+    local x = {...}
+    print(unpack(x))
+    local tobj = { delay = delay, f = fn, rp = 0, args = {...} }
+    if (delay <= tick or delay > 2147483646) then
+        tobj.delay = tick
         table.insert(exeque, tobj)
-        print('set timeout 4')
     else
-        print('set timeout 5')
         table.insert(ttbl, tobj)
-        print('set timeout 6')
     end
-    print('set timeout 7')
     if (timer.enable == false) then timer.start() end
-    print('set timeout 8')
     return tobj
 end
 
 function timer.setInterval(fn, delay, ...)
-    print('set interval 1')
     local tobj = timer.setTimeout(fn, delay, ...)
-    print('set interval 2')
     tobj.rp = delay
-    print('set interval 3')
     if (timer.enable == false) then timer.start() end
-    print('set interval 4')
     return tobj
 end
 
@@ -148,8 +128,8 @@ function timer.clearImmediate(tobj)
 end
 
 function timer.clearTimeout(tobj)
-    exeque = rmEntry(exeque, tobj)
-    ttbl = rmEntry(ttbl, tobj)
+    rmEntry(exeque, tobj)
+    rmEntry(ttbl, tobj)
 end
 
 timer.clearInterval = timer.clearTimeout
